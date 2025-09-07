6d4b94f8a0677bd84e91bd2caf48dfdd17a17db9
Boggarapu Venkata Srujana <bvsrujana99@gmail.com>
2025-09-07 22:04:03 +0530

    Add files via upload

---
+++ setBit.java
@@ -0,0 +1,12 @@
+public class setBit {
+    //Set the 2nd bit(position=1) of a number n(n=0101)
+    //(i).Bit Mask:1<<i
+    //(ii).Operation:OR
+    public static void main(String args[]){
+        int n=5;
+        int pos=1;
+        int bitmask=1<<pos;
+        int newNumber=bitmask | n;
+        System.out.println(newNumber);
+    }
+}
